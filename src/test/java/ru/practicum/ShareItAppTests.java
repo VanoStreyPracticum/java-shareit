0451c52b98acf737df84d3660887438fde23b1a5
VanoStreyPracticum <vanostrey@yandex.com>
2025-12-20 04:31:41 +0300

    config test update

---
+++ src/test/java/ru/practicum/ShareItAppTests.java
@@ -0,0 +1,14 @@
+package ru.practicum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ShareItAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
